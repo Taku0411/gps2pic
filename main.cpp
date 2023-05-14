e2fa4a85184cf792e9e3bd87010cdad1be0e642b
Taku0411 <19991104taku@gmail.com>
2023-05-14 10:20:03 +0900

    libOK

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ int main(int argc, char *argv[])
   for (auto &item: paths)
   {
     if (item.extension() == ".jpeg") {}
-    else if (item.extension() == ".CR2")
+    else if (item.extension() == ".CR2" || item.extension() == ".JPG")
     {
 
-      Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(item.string());
+      Exiv2::Image::UniquePtr image = Exiv2::ImageFactory::open(item.string());
       image->readMetadata();
       Exiv2::ExifData &exifData = image->exifData();
 
       exifData["Exif.GPSInfo.GPSAltitude"] = std::string("4/1 15/1 33/1");
-      exifData["Exif.GPSInfo.GPSAltitudeRef"] = std::string("N");
+      exifData["Exif.GPSInfo.GPSLatitude"] = std::string("4/1 15/1 33/1");
+      exifData["Exif.GPSInfo.GPSLongitude"] = std::string("4/1 15/1 33/1");
 
       image->writeMetadata();
     }
